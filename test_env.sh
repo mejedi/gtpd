ad917b1e154f50eeeafb0baa795aa74e4eeefc15
Nick Zavaritsky <mejedi@gmail.com>
2022-03-21 11:05:29 +0000

    test_env: suppress output from ethtool

--- test_env.sh
+++ test_env.sh
@@ -4,7 +4,8 @@
 # gtpd_ctl to attach gtpd-tap.
 unshare -rUn sh -c '
 ip link add gtpd-tap type veth peer name eth0;
-ethtool -K eth0 tx off; ip link set dev lo up;
+ethtool -K eth0 tx off > /dev/null;
+ip link set dev lo up;
 ip link set dev gtpd-tap arp off up;
 ip link set dev eth0 arp off address 00:00:00:00:00:01 mtu 1400 up;
 ip route add default dev eth0;
